f5f4884133f7558c82b51432bba6431eb4a12d76
sudarshan238 <sudarshanyadav5678@gmail.com>
2025-08-22 12:41:40 +0530

    day-10

---
+++ Day-10-import/main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "name" {
+    ami = "ami-01102c5e8ab69fb75"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "sudha"
+    }
+  
+}
+resource "aws_s3_bucket" "name" {
+    bucket = "awssddev"
+  
+}
+resource "aws_iam_user" "admin_user" {
+  name = "yadav"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_access" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+  
